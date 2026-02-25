978e15d5a280479603bee669a4625c7f1cf2bb4d
guilhermeileite <guilhermekpr@gmail.com>
2026-02-25 16:53:45 -0300

    Project created

---
+++ src/main/java/com/guilhermeleite/ntdlist/NtdlistApplication.java
@@ -0,0 +1,13 @@
+package com.guilhermeleite.ntdlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtdlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NtdlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilhermeleite/ntdlist/NtdlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermeleite.ntdlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtdlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
